faeff12657ce83725fbd340405bfbfd9327a62a4
Geoff Stilwell <gstilwell@gmail.com>
2021-05-26 22:14:05 -0400

    running in the browser!

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,6 +1,179 @@
 #![cfg(target_arch = "wasm32")]
 
-extern crate rg3d;
+use rg3d::{
+    core::{
+        algebra::{UnitQuaternion, Vector3},
+        pool::Handle,
+    },
+    engine::{resource_manager::ResourceManager, Engine},
+    event::{DeviceEvent, ElementState, Event, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    gui::node::StubNode,
+    physics::{dynamics::RigidBodyBuilder, geometry::ColliderBuilder},
+    resource::texture::TextureWrapMode,
+    scene::{
+        base::BaseBuilder,
+        camera::{CameraBuilder, SkyBox},
+        node::Node,
+        transform::TransformBuilder,
+        Scene,
+    },
+    window::WindowBuilder,
+};
+use std::{
+    time,
+    panic,
+};
 
-fn main() {
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn error(msg: String);
+
+    type Error;
+
+    #[wasm_bindgen(constructor)]
+    fn new() -> Error;
+
+    #[wasm_bindgen(structural, method, getter)]
+    fn stack(error: &Error) -> String;
+}
+
+fn hook_impl(info: &panic::PanicInfo) {
+    let mut msg = info.to_string();
+
+    // Add the error stack to our message.
+    //
+    // This ensures that even if the `console` implementation doesn't
+    // include stacks for `console.error`, the stack is still available
+    // for the user. Additionally, Firefox's console tries to clean up
+    // stack traces, and ruins Rust symbols in the process
+    // (https://bugzilla.mozilla.org/show_bug.cgi?id=1519569) but since
+    // it only touches the logged message's associated stack, and not
+    // the message's contents, by including the stack in the message
+    // contents we make sure it is available to the user.
+    msg.push_str("\n\nStack:\n\n");
+    let e = Error::new();
+    let stack = e.stack();
+    msg.push_str(&stack);
+
+    // Safari's devtools, on the other hand, _do_ mess with logged
+    // messages' contents, so we attempt to break their heuristics for
+    // doing that by appending some whitespace.
+    // https://github.com/rustwasm/console_error_panic_hook/issues/7
+    msg.push_str("\n\n");
+
+    // Finally, log the panic with `console.error`!
+    error(msg);
+}
+
+/// A panic hook for use with
+/// [`std::panic::set_hook`](https://doc.rust-lang.org/nightly/std/panic/fn.set_hook.html)
+/// that logs panics into
+/// [`console.error`](https://developer.mozilla.org/en-US/docs/Web/API/Console/error).
+///
+/// On non-wasm targets, prints the panic to `stderr`.
+pub fn hook(info: &panic::PanicInfo) {
+    hook_impl(info);
+}
+
+/// Set the `console.error` panic hook the first time this is called. Subsequent
+/// invocations do nothing.
+#[inline]
+pub fn set_once() {
+    use std::sync::Once;
+    static SET_HOOK: Once = Once::new();
+    SET_HOOK.call_once(|| {
+        panic::set_hook(Box::new(hook));
+    });
+}
+
+// Create our own engine type aliases. These specializations are needed, because the engine
+// provides a way to extend UI with custom nodes and messages.
+type GameEngine = Engine<(), StubNode>;
+
+// Our game logic will be updated at 60 Hz rate.
+const TIMESTEP: f32 = 1.0 / 60.0;
+
+struct Game {
+    // Empty for now.
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn update(&mut self) {
+        // Game logic will be placed here.
+    }
+}
+
+#[wasm_bindgen]
+pub fn main() {
+    set_once();
+
+    // Configure main window first.
+    let window_builder = WindowBuilder::new().with_title("Gorust!");
+    // Create event loop that will be used to "listen" events from the OS.
+    let event_loop = EventLoop::new();
+
+    // Finally create an instance of the engine.
+    let mut engine = GameEngine::new(window_builder, &event_loop, true).unwrap();
+
+    // Initialize game instance. It is empty for now.
+    let mut game = Game::new();
+
+    // Run the event loop of the main window. which will respond to OS and window events and update
+    // engine's state accordingly. Engine lets you to decide which event should be handled,
+    // this is minimal working example if how it should be.
+    let clock = rg3d::core::instant::Instant::now();
+
+    let mut elapsed_time = 0.0;
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::MainEventsCleared => {
+                // This main game loop - it has fixed time step which means that game
+                // code will run at fixed speed even if renderer can't give you desired
+                // 60 fps.
+                let mut dt = clock.elapsed().as_secs_f32() - elapsed_time;
+                while dt >= TIMESTEP {
+                    dt -= TIMESTEP;
+                    elapsed_time += TIMESTEP;
+
+                    // Run our game's logic.
+                    game.update();
+
+                    // Update engine each frame.
+                    engine.update(TIMESTEP);
+                }
+
+                // Rendering must be explicitly requested and handled after RedrawRequested event is received.
+                engine.get_window().request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                // Render at max speed - it is not tied to the game code.
+                engine.render(TIMESTEP).unwrap();
+            }
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput { input, .. } => {
+                    // Exit game by hitting Escape.
+                    if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
+                        *control_flow = ControlFlow::Exit
+                    }
+                }
+			    WindowEvent::Resized(size) => {
+                    // It is very important to handle Resized event from window, because
+                    // renderer knows nothing about window size - it must be notified
+                    // directly when window size has changed.
+                    engine.renderer.set_frame_size(size.into());
+                }
+                _ => (),
+            },
+            _ => *control_flow = ControlFlow::Poll,
+        }
+    });
 }
